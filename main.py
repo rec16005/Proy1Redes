f0f2f92739a8736bc5e848de2d61e8fc4ce0bcad
rec16005 <rec16005@uvg.edu.gt>
2019-07-31 10:49:10 -0600

    chat 1 a 1 y listar usuarios

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ import getpass
 import sleekxmpp
 from optparse import OptionParser
 from sleekxmpp.exceptions import IqError, IqTimeout
+import ssl
 
-
-class Register(sleekxmpp.ClientXMPP):
+class Chat(sleekxmpp.ClientXMPP):
 
     def __init__(self, jid, password):
         sleekxmpp.ClientXMPP.__init__(self, jid, password)
 
-        #Evento de registro
+        #Event Handlers 
+        #register
         self.add_event_handler("register", self.register)
-        #Evento de start
+        #start
         self.add_event_handler("session_start", self.start)
+        #message
+        self.add_event_handler("message", self.message, threaded=True)
 
-        #Procesa el evento session_start
     def start(self, event):
         print('Session start')
         self.send_presence()
@@ -30,117 +32,43 @@ class Register(sleekxmpp.ClientXMPP):
 
         try:
             resp.send(now=True)
-            logging.info("Se creo la cuenta: %s!" % self.boundjid)
+            logging.info("Account Created: %s!" % self.boundjid)
         except IqError as e:
-            logging.error("No se pudo registrar la cuenta %s" %
+            logging.error("Unable to register: %s" %
                     e.iq['error']['text'])
             self.disconnect()
         except IqTimeout:
-            logging.error("No hubo respuesta del servidor")
+            logging.error("No response from the server")
             self.disconnect()
 
-
-class EchoBot(sleekxmpp.ClientXMPP):
-
-    def __init__(self, jid, password):
-
-        sleekxmpp.ClientXMPP.__init__(self, jid, password)
-
-
-        #Evento de start
-        self.add_event_handler("session_start", self.start)
-        #Evento de envio de mensaje
-       
-        self.add_event_handler("message", self.message)
-        
-
-
-    #Procesa el evento session_start
-    def start(self, event):
-        print('Session start')
-        self.send_presence()
-        self.get_roster()
-
-
-    #Procesa los mensajes entrantes 
+    def remove_user(self):
+        resp = self.Iq()
+        resp['type'] = 'set'
+        resp['from'] = self.boundjid.user
+        resp['register'] = ' '
+        resp['register']['remove'] = ' '
+        try:
+            print(resp)
+            resp.send(now=True)
+            print("Account deleted: %s" % self.boundjid)
+        except IqError as e:
+            logging.error("Unable to delete account: %s" %
+                    e.iq['error']['text'])
+            self.disconnect()
+        except IqTimeout:
+            logging.error("No response from the server")
+            self.disconnect()
+    
     def message(self, msg):
         if msg['type'] in ('chat', 'normal'):
-            msg.reply("Se envio\n%(body)s" % msg).send()
-            print(msg['body'])
+            print(str(msg['from']) + ": " + msg['body'])
 
-
-
-    def send_message(self, recipient, msg):
-        self.recipient = recipient
-        self.msg = msg
-        self.send_message(mto=self.recipient,
-                          mbody=self.msg,
-                          mtype='chat')
-    def group_message():
-        pass
-
-    #Nos imprime todos los usuarios
-    def get_users(self):
-        print(self.get_roster())
-
-    def exit(self):
-        self.disconnect()
-    
-class SendMsgBot(sleekxmpp.ClientXMPP):
-
-    """
-    A basic SleekXMPP bot that will log in, send a message,
-    and then log out.
-    """
-
-    def __init__(self, jid, password, recipient, message):
-        sleekxmpp.ClientXMPP.__init__(self, jid, password)
-
-        # The message we wish to send, and the JID that
-        # will receive it.
-        self.recipient = recipient
-        self.msg = message
-
-        # The session_start event will be triggered when
-        # the bot establishes its connection with the server
-        # and the XML streams are ready for use. We want to
-        # listen for this event so that we we can initialize
-        # our roster.
-        self.add_event_handler("session_start", self.start)
-
-    def start(self, event):
-        """
-        Process the session_start event.
-
-        Typical actions for the session_start event are
-        requesting the roster and broadcasting an initial
-        presence stanza.
-
-        Arguments:
-            event -- An empty dictionary. The session_start
-                     event does not provide any additional
-                     data.
-        """
-        self.send_presence()
-        self.get_roster()
-
-        self.send_message(mto=self.recipient,
-                          mbody=self.msg,
-                          mtype='chat')
-
-        # Using wait=True ensures that the send queue will be
-        # emptied before ending the session.
-        self.disconnect(wait=True)
 
 if __name__ == '__main__':
 
-    print('1. Login')
-    print('2. Register')
-    x = input('Ingrese el número de la opción que desea: \n') 
-
     optp = OptionParser()
 
-    #Opciones de output
+    #Debugging stuff
     optp.add_option('-q', '--quiet', help='set logging to ERROR',
                     action='store_const', dest='loglevel',
                     const=logging.ERROR, default=logging.INFO)
@@ -150,12 +78,12 @@ if __name__ == '__main__':
     optp.add_option('-v', '--verbose', help='set logging to COMM',
                     action='store_const', dest='loglevel',
                     const=5, default=logging.INFO)
-    #Opciones de JID y password .
+
+    #User Stuff
     optp.add_option("-j", "--jid", dest="jid",
                     help="JID to use")
     optp.add_option("-p", "--password", dest="password",
                     help="password to use")
-    #Mensaje y a quien
     optp.add_option("-t", "--to", dest="to",
                     help="JID to send the message to")
     optp.add_option("-m", "--message", dest="message",
@@ -163,51 +91,71 @@ if __name__ == '__main__':
 
     opts, args = optp.parse_args()
 
-    #Setear el login
+    #logging config
     logging.basicConfig(level=opts.loglevel,
                         format='%(levelname)-8s %(message)s')
+
+    print('Type 1 to Login')
+    print('Type 2 to Register')
+    x = input(': ') 
 
     if opts.jid is None:
         opts.jid = input("Username: " )
     if opts.password is None:
         opts.password = getpass.getpass("Password: ")
 
-    if(x =='1'):
-        #Setup de mi clase EchoBot
-        print("Se esta iniciando sesion")
-        xmpp = EchoBot(opts.jid, opts.password)
-    
-    elif(x=='2'):
-        print("Se esta registrando")
-        xmpp = Register(opts.jid, opts.password)
+    xmpp = Chat(opts.jid, opts.password)
 
+    if(x=='1'):
+        xmpp.del_event_handler("register", xmpp.register)
 
-    if opts.to is None:
-        opts.to = input("Send To: ")
-    if opts.message is None:
-        opts.message = input("Message: ")
-    xmpp = SendMsgBot(opts.jid, opts.password, opts.to, opts.message)
-
-    
-
-        
+    #plugins
     xmpp.register_plugin('xep_0030') # Service Discovery
     xmpp.register_plugin('xep_0199') # XMPP Ping
     xmpp.register_plugin('xep_0004') # Data forms
     xmpp.register_plugin('xep_0066') # Out-of-band Data
     xmpp.register_plugin('xep_0077') # In-band Registration
-        
-
-    xmpp['xep_0077'].force_registration = True
-
+    xmpp.register_plugin('xep_0060') # PubSub
  
-    
+    xmpp['feature_mechanisms'].unencrypted_plain = True
+    xmpp.ssl_version = ssl.PROTOCOL_TLS
 
     #Conexion con el server
-    if xmpp.connect():
+    if xmpp.connect(('alumchat.xyz', 5222)):
         
-        xmpp.process(block=True)
-        print("Done")
+        xmpp.process(block=False)
+        while True:
+            print('Press:')
+            print('1 to disconnect')
+            print('2 to delete account from server')
+            print('3 to list users')
+            print('4 to send a message')
+            y = input(": ")
+
+            #disconnect
+            if(y == '1'):
+                print('goodbye')
+                xmpp.disconnect()
+                break
+            
+            #delete account
+            elif(y == '2'):
+                xmpp.remove_user()
+                xmpp.disconnect()
+                break
+            
+            #list users
+            elif(y == '3'):
+                print('List of Users:')
+                print(xmpp.client_roster)
+                print(' ')
+            
+            #send message
+            elif(y == '4'):
+                usr = input('Who do you want to send a message?')
+                mssg = input('What message do you want to send?')
+                print('Sending...')
+                xmpp.send_message(mto=usr, mbody=mssg, mtype='chat')
        
     else:
         print("Unable to connect.")
